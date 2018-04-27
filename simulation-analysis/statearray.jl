f3fe957055883db7ccc68e41bdc8ede819ca48ba
marcelovmaciel <marcelovmaciel@protonmail.com>
2018-04-26 21:24:41 -0300

    multipanel plot; let's see if my current config can handle lool

--- simulation-analysis/statearray.jl
+++ simulation-analysis/statearray.jl
@@ -5,22 +5,56 @@ using Plots
 using StatsBase, LaTeXStrings
 inspectdr(legend = false)
 
-fig = plot(show = false, xlabel = "iterações", ylabel = "valor do ponto ideal"  )
+
+# pre-plots
+
+#finalplot = plot(figs[1], figs[2])
+
+σs = [ 0.02 0.05  0.1]
+
+parametrizations = [dd.DyDeoParam(n_issues = 7, σ = σ, size_nw = 500,
+                                  time = 500_000, p = 0.7,
+                                  ρ = 0.0, propintransigents = 0.0) for σ in σs]
 
 
-pa = dd.DyDeoParam(n_issues = 1, σ = 0.02, size_nw = 500, time = 500_000,
-                   ρ = 0.0, propintransigents = 0.15)
+figs = [plot(show = false, xlabel = "iterações",
+             ylabel = "valor do ponto ideal", title = "$(i)"  ) for i in 1:length(parametrizations)]
 
-statevec = dd.simstatesvec(pa);
+# pa = dd.DyDeoParam(n_issues = 7, σ = 0.05, size_nw = 500, time = 100_000, p = 0.7,
+ #                  ρ = 0.0, propintransigents = 0.0)
 
-a = dd.statesmatrix(statevec, pa.time, pa.size_nw);
+#runs arrays
+parametrizations_to_states = map(dd.statesmatrix,parametrizations)
 
+
+for i in zip(parametrizations,figs,parametrizations_to_states)
+dd.@showprogress 1 "Plotting "    for j in 1:i[1].size_nw
+    plot!(i[2],i[3][:,j])
+    end
+end
+
+plot(figs[1],figs[2],figs[3])
+
+
+savefig("image/testmulti.png")
+#finalplot = plot(figs[1],figs[2],figs[3])
+
+#=
 a[1,:] |> std
-
-round.(a[pa.time,:], 5) |> countmap
 
 a[pa.time,:]  |> std
 
+round.(a[pa.time,:], 6) |> countmap
+
+round.(a[1,:], 6) |> countmap
+
+a[pa.time,:] |> countmap
+=#
+
+
+#plots->pnn
+
+#=
 
 dd.@showprogress 1 "Plotting " for i in 1:pa.size_nw
     plot!(fig,a[:,i])
@@ -30,8 +64,5 @@ end
 #display(fig)
 #closeall()
 
-savefig("image/timeseries4.png")
 
-
-
-
+=#
