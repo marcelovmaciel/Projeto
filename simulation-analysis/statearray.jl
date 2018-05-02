4b1e27fbdd18af777066828a40078351344274f9
Marcelo Maciel <marcelovmaciel@protonmail.com>
2018-05-02 19:42:59 -0300

    minor code changes; gotta finish this but lacking willpower

--- simulation-analysis/statearray.jl
+++ simulation-analysis/statearray.jl
@@ -2,42 +2,44 @@ include("../simulation_scripts/DyDeo2/src/DyDeo2.jl")
 import DyDeo2
 const dd = DyDeo2
 using Plots
-using StatsBase, LaTeXStrings
+using StatsBase
 inspectdr(legend = false)
-
+ 
 
 # pre-plots
 
 #finalplot = plot(figs[1], figs[2])
 
-σs = [ 0.02 0.05  0.1]
+fig = plot(show = false, xlabel = "iterações", ylabel = "valores dos pontos ideais")
 
-parametrizations = [dd.DyDeoParam(n_issues = 7, σ = σ, size_nw = 500,
+
+pa = dd.DyDeoParam(n_issues = 1, σ = 0.02, size_nw = 500,
                                   time = 500_000, p = 0.7,
-                                  ρ = 0.0, propintransigents = 0.0) for σ in σs]
+                                  ρ = 0.0, propintransigents = 0.0)
+
+pa_to_states = dd.statesmatrix(pa)
 
 
-figs = [plot(show = false, xlabel = "iterações",
-             ylabel = "valor do ponto ideal", title = "$(i)"  ) for i in 1:length(parametrizations)]
+dd.@showprogress 1 "Plotting " for i in 1:pa.size_nw
+    plot!(fig, pa_to_states[:,i])
+end
 
-# pa = dd.DyDeoParam(n_issues = 7, σ = 0.05, size_nw = 500, time = 100_000, p = 0.7,
- #                  ρ = 0.0, propintransigents = 0.0)
+png("image/test1kk")
+println("done")
 
 #runs arrays
-parametrizations_to_states = map(dd.statesmatrix,parametrizations)
+# parametrizations_to_states = map(dd.statesmatrix,parametrizations)
 
 
+
+#=
 for i in zip(parametrizations,figs,parametrizations_to_states)
-dd.@showprogress 1 "Plotting "    for j in 1:i[1].size_nw
+dd.@showprogress 1 "Plotting " for j in 1:i[1].size_nw
     plot!(i[2],i[3][:,j])
     end
 end
 
-plot(figs[1],figs[2],figs[3])
-
-
-savefig("image/testmulti.png")
-#finalplot = plot(figs[1],figs[2],figs[3])
+=#
 
 #=
 a[1,:] |> std
