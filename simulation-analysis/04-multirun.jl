eca1028009384dcb6d9bb3ad186a45d1f9a83be4
Marcelo Maciel <marcelovmaciel@protonmail.com>
2018-05-05 19:45:37 -0300

    probably gonna turn the array into a df so that i can plot with statplots

--- simulation-analysis/04-multirun.jl
+++ simulation-analysis/04-multirun.jl
@@ -4,16 +4,24 @@ const dd = DyDeo2
 using StatsBase, IterTools, Plots
 
 
-test = (let
-          σs = [ 0.02 0.04 0.06 0.1 0.14]
-          n_issues =  [1 5 10]
-          product(n_issues, σs) |> collect |> sort |>
-          x->partition(x, length(σs)) |>collect
-          end) 
-
-Ystd, Ynips = test[1][5] |> dd.multiruns |> dd.extractys
+σs = [ 0.02 0.04 0.06 0.1 0.14]
+n_issues =  [1 5 10]
 
 
-pyplot()
+test = (product(n_issues, σs) |> collect |> sort |>
+        x->partition(x, length(σs)) |>collect)
+       
 
-plot([mean(Ynips)], yerr = std(Ynips)) 
+results = map(x -> (x |> dd.multiruns |> dd.extractynips), test[1])
+
+
+fig = plot()
+
+for i in results
+    boxplot!(fig,i)
+end
+
+
+
+fig
+closeall()
