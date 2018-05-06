53b5cdd045e0ac16fa3289b1e81fdd0f717dcae7
Marcelo Maciel <marcelovmaciel@protonmail.com>
2018-05-06 18:16:53 -0300

    break data generation from plotting

--- simulation-analysis/04-multirun.jl
+++ simulation-analysis/04-multirun.jl
@@ -1,27 +1,46 @@
 include("../simulation_scripts/DyDeo2/src/DyDeo2.jl")
 import DyDeo2
 const dd = DyDeo2
-using StatsBase, IterTools, Plots
-
+using StatsBase, IterTools, Plots, DataFrames, JLD2
+pyplot()
 
 σs = [ 0.02 0.04 0.06 0.1 0.14]
 n_issues =  [1 5 10]
 
-
+
+# Generate the data
 test = (product(n_issues, σs) |> collect |> sort |>
         x->partition(x, length(σs)) |>collect)
        
 
-results = map(x -> (x |> dd.multiruns |> dd.extractynips), test[1])
+test_results = []
 
-
-fig = plot()
-
-for i in results
-    boxplot!(fig,i)
+for i in 1:length(test)    
+    (map(x -> (x |> dd.multiruns |> dd.extractynips), test[i]) |>
+     x -> (push!(test_results,x)))
 end
 
+@save "data/multi-out.jld2" test_results
 
+
+
+
+
+
+
+#=
+issuestodf = append!(n_issues', [missing, missing])
+
+df = DataFrame(σ = σs, n_issues  = [n_issues, missing, missing])
+=#
+
+
+
+fig = plot(show = false)
+
+for i in results
+    boxplot!(fig,i, legend = false)
+end
 
 fig
 closeall()
