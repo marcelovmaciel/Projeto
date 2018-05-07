1dfac1e5d4077981a6e359b6f676b85adaca8c4b
Marcelo Maciel <marcelovmaciel@protonmail.com>
2018-05-07 12:22:02 -0300

    i'll settle with this plot

--- simulation-analysis/04-multirun.jl
+++ simulation-analysis/04-multirun.jl
@@ -4,7 +4,7 @@ const dd = DyDeo2
 using StatsBase, IterTools, Plots, DataFrames, JLD2
 pyplot()
 
-σs = [ 0.02 0.04 0.06 0.1 0.14]
+σs = [ 0.02, 0.04, 0.06, 0.1, 0.14]
 n_issues =  [1 5 10]
 
 
@@ -23,31 +23,3 @@ end
 @save "data/multi-out.jld2" test_results
 
 
-#Plot the data now
-
-@load "data/multi-out.jld2"
-
-
-#=
-issuestodf = append!(n_issues', [missing, missing])
-
-df = DataFrame(σ = σs, n_issues  = [n_issues, missing, missing])
-=#
-
-fig1 = plot(show = false)
-fig2 = plot(show = false)
-fig3 = plot(show = false)
-
-boxmulti(results,fig) = [boxplot!(fig,i , legend = false) for i in results]
-
-boxmulti(test_results[1],fig1)
-boxmulti(test_results[2],fig2)
-boxmulti(test_results[3],fig3)
-
-finalfig = plot(fig1,fig2,fig3,layout = grid(3,1))
-closeall()
-
-
-testdict = Dict(0.1 => [1 2 3 4], 0.2 => "fool")
-
-testdict[0.2]
