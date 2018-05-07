cff48edc6363ebe9d50202bc0bc4f55fa273f3d6
Marcelo Maciel <marcelovmaciel@protonmail.com>
2018-05-07 16:30:13 -0300

    finished plots; now write a little

--- simulation-analysis/04-multirun.jl
+++ simulation-analysis/04-multirun.jl
@@ -5,7 +5,7 @@ using StatsBase, IterTools, Plots, DataFrames, JLD2
 pyplot()
 
 σs = [ 0.02, 0.04, 0.06, 0.1, 0.14]
-n_issues =  [1 5 10]
+n_issues =  [1 5 10 20]
 
 
 # Generate the data
@@ -20,6 +20,6 @@ for i in 1:length(test)
      x -> (push!(test_results,x)))
 end
 
-@save "data/multi-out.jld2" test_results
+@save "data/multi-out2.jld2" test_results
 
 
