fe3d9695852afcfe1ec0c069a83515d81764e400
Marcelo Maciel <marcelovmaciel@protonmail.com>
2018-05-09 17:00:15 -0300

    ok, all done; now discuss new cases with andre

---
+++ simulation-analysis/05-statearray.jl
@@ -0,0 +1,80 @@
+include("../simulation_scripts/DyDeo2/src/DyDeo2.jl")
+import DyDeo2
+const dd = DyDeo2
+using Plots
+using StatsBase
+inspectdr(legend = false)
+
+
+
+# pre-plots
+
+#finalplot = plot(figs[1], figs[2])
+
+fig = plot(show = false, xlabel = "iterações",
+           ylabel = "valores dos pontos ideais",
+           title = "n = 1 ; sigma = 0.1")
+
+#testar σ 0.02 0.04 0.06  e 0.1
+
+pa = dd.DyDeoParam(n_issues = 1, σ = 0.02,
+                   size_nw = 500,
+                   time = 500_000,
+                   p = 0.9,
+                   ρ = 0.05,
+                   propintransigents = 0.15,
+                   intranpositions = "random")
+
+pa_to_states = dd.statesmatrix(pa)
+
+#  pa_to_states[end,  :] |>  x -> round.(x,5) |> dd.outputfromsim
+
+
+dd.@showprogress 1 "Plotting " for i in 1:pa.size_nw
+    plot!(fig, pa_to_states[:,i])
+end
+
+png("image/testn1-002")
+println("done")
+
+#runs arrays
+# parametrizations_to_states = map(dd.statesmatrix,parametrizations)
+
+
+
+#=
+for i in zip(parametrizations,figs,parametrizations_to_states)
+dd.@showprogress 1 "Plotting " for j in 1:i[1].size_nw
+    plot!(i[2],i[3][:,j])
+    end
+end
+
+=#
+
+#=
+a[1,:] |> std
+
+a[pa.time,:]  |> std
+
+round.(a[pa.time,:], 6) |> countmap
+
+round.(a[1,:], 6) |> countmap
+
+a[pa.time,:] |> countmap
+=#
+
+
+#plots->pnn
+
+#=
+
+dd.@showprogress 1 "Plotting " for i in 1:pa.size_nw
+    plot!(fig,a[:,i])
+end
+
+
+#display(fig)
+#closeall()
+
+
+=#
