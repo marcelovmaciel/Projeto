8564639346f7ac285fa95b85db6ce08c6c1fd1d1
Marcelo Maciel <marcelovmaciel@protonmail.com>
2018-06-04 16:23:21 -0300

    run tests again, plz be the last

--- simulation-analysis/01-samplerun.jl
+++ simulation-analysis/01-samplerun.jl
@@ -1,34 +1,31 @@
 include("../simulation_scripts/DyDeo2/src/DyDeo2.jl")
 
 using DyDeo2, PyCall
+const dd = DyDeo2
+
 
 
 @pyimport SALib.sample.saltelli as saltelli
 
-mkdirs("data")
-
-
-problem  = Dict("num_vars" => 6,
-             "names" => ["size_nw", "n_issues", "p", "σ", "ρ", "p_intran"],
-             "bounds" => [[500, 5_000],
-                          [1, 10],
+problem  = Dict("num_vars" => 5,
+             "names" => [ "n_issues", "p", "σ", "ρ", "p_intran"],
+             "bounds" => [[1, 10],
                           [0.1, 0.99],
                           [0.01, 0.5],
                           [0.0, 0.1],
-                          [0.0, 0.1]])
+                          [0.0, 0.3]])
 
-param_values_saltelli5000 = saltelli.sample(problem, 5000)
-
-
-@save  "data/saltellisample5000.jld2" param_values_saltelli5000
+paramvalues5k_5params = saltelli.sample(problem, 5000)
 
 
-Ysaltelli5000 = sweep_sample(param_values_saltelli5000,
-                             time = 1_000_000,
-                             agent_type = "mutating o")
+dd.@save  "data/sample5k5params.jld2" paramvalues5k_5params
+
+
+Ysaltelli5params = dd.sweep_sample(paramvalues5k_5params,
+                                time = 1_000_000,
+                                size_nw = 500,
+                                agent_type = "mutating o")
+
+dd.@save "data/saltelli5k5params.jld2" Ysaltelli5params
 
 println("done")
-
-@save "data/saltellioutput5000.jld2" Ysaltelli5000
-
-
